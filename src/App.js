b82f07833fc526b539f2116fa8b136a252097ae4
Patrick hachicho <Hachichopatrick@gmail.com>
2022-11-06 11:14:05 -0500

    learned bootstrap with react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import './App.css';
-
-
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import NavBar from "./components/NavBar";
+import Form from "react-bootstrap/Form";
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1>Bootstrap Website</h1>
-     
+      <Button variant="outline-info">Info</Button>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
